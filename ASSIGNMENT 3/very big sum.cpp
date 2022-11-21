5b1accbcf5e592e04d05e115c3bac3d99e458aa7
Danish-2003 <115136828+Danish-2003@users.noreply.github.com>
2022-11-21 22:42:07 +0530

    Create very big sum.cpp

---
+++ ASSIGNMENT 3/very big sum.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int input;
+    cin>>input;
+    long sum=0;
+    for(int i=0;i<input;i++){
+        
+        long a;
+        cin>>a;
+        sum=sum+a;
+    }
+    cout<<sum;
+    return 0;
+}
