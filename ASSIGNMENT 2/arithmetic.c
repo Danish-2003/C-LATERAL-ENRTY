cebe71840afe06e10560693f9621cdba1efaa68b
Danish-2003 <115136828+Danish-2003@users.noreply.github.com>
2022-11-21 22:27:39 +0530

    Create arithmetic.c

---
+++ ASSIGNMENT 2/arithmetic.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a,b;
+    float c,d;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%f",&c);
+    scanf("%f",&d);
+    int sumi=a+b,subi=a-b;
+    float sumf=c+d,subf=c-d;
+    printf("%d",sumi);
+    printf(" ");
+    printf("%d",subi);
+    printf("\n");
+    printf("%0.1f",sumf);
+    printf(" ");
+    printf("%0.1f",subf);
+
+    
+    return 0;
+}
