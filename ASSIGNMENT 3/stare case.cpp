7286daee6ac8f6306201db16ca7cd4a5b526736b
Danish-2003 <115136828+Danish-2003@users.noreply.github.com>
2022-11-21 22:40:36 +0530

    Create stare case.cpp

---
+++ ASSIGNMENT 3/stare case.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for(int k=n-i-1;k<n;k++){
+            cout<<"#";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
